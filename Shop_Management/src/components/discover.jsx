9acfecedd1f15a301c4d3f00d249c81bc0bf2c38
LakshMe <pmenonlakshmi.05@gmail.com>
2025-06-13 14:48:27 +0530

    .

--- Shop_Management/src/components/discover.jsx
+++ Shop_Management/src/components/discover.jsx
@@ -629,7 +629,7 @@ useEffect(() => {
                         ? 'Your Product'
                         : 'Buy Now'}
                       </button>
-
+                      {!isOwnProduct(product) &&(
                       <button
                         className="wishlist-btn"
                         onClick={() => toggleWishlist(product)}
@@ -638,7 +638,7 @@ useEffect(() => {
                         <svg width="18" height="18" viewBox="0 0 24 24" fill={isInWishlist(product.id) ? 'red' : 'none'} stroke="currentColor" strokeWidth="2">
                           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                         </svg>
-                      </button>
+                      </button>)}
                     </div>
                   </div>
                 </div>
@@ -754,7 +754,7 @@ setBuyQuantity(Number(product.minOrder) || 1);
                         : 'Buy Now'}
                       </button>
 
-                      <button
+                     {!isOwnProduct(produtc) &&( <button
                         className="wishlist-btn"
                         onClick={() => toggleWishlist(product)}
                         title={isInWishlist(product.id) ? 'Remove from Wishlist' : 'Add to Wishlist'}
@@ -762,7 +762,7 @@ setBuyQuantity(Number(product.minOrder) || 1);
                         <svg width="18" height="18" viewBox="0 0 24 24" fill={isInWishlist(product.id) ? 'red' : 'none'} stroke="currentColor" strokeWidth="2">
                           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                         </svg>
-                      </button>
+                      </button>)}
                     </div>
                   </div>
                 </div>
